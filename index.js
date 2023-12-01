48a986b8fb947747c85cc96bb92f1bac5055dae8
Mariele <102813740+ladyMariele@users.noreply.github.com>
2023-12-01 22:19:32 +0800

    remove 'onclick' attribute. add addeventlistener

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const inputEl = document.getElementById("input-field")
 const listEl = document.getElementById("item")
+const buttonEl = document.getElementById("button")
 
 
+buttonEl.addEventListener("click", function() {
 
-function add() {
     if (inputEl.value == "") {
-        alert("You forgot to enter an item.")
-    } else {
-        listEl.innerHTML += `
-            <li onclick="this.style.display='none'">
-            ${inputEl.value}
-            </li>
-        ` 
-      }
-    
+            alert("You forgot to enter an item.")
+        } else {
+            listEl.innerHTML += `
+                <li onclick="this.style.display='none'">
+                ${inputEl.value}
+                </li>
+            ` 
+            }
     inputEl.setAttribute("autofocus", "autofocus")
     inputEl.focus() // this actually sets the autofocus attribute
 
     inputEl.value = ""
-}
+
+})
